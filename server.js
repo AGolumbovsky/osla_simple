348fa1e18eb4949cac1a4ad3c4bdd71e84467d9c
Andriy Golumbovsky <AGolumbovsky@gmail.com>
2018-08-27 16:21:04 -0400

    second commit

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
-    console.log("osla-s is listenign on port" + PORT);
+    console.log("osla-s is listening on port" + PORT);
 })
