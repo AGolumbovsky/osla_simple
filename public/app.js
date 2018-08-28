5a6bee202de66d42372d944780ce47aca5992b6a
Andriy Golumbovsky <AGolumbovsky@gmail.com>
2018-08-28 14:38:21 -0400

    push them all

--- public/app.js
+++ public/app.js
@@ -22,12 +22,15 @@ let getIt = () => {
 var url = 'https://example.com/profile';
 var data = {username: 'example'};
 
-fetch(url, {
-  method: 'POST', // or 'PUT'
-  body: JSON.stringify(data), // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/json'
-  }
-}).then(res => res.json())
-.then(response => console.log('Success:', JSON.stringify(response)))
-.catch(error => console.error('Error:', error));
+let postIt = () => {
+    
+    fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+        'Content-Type': 'application/json'
+    }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+}
