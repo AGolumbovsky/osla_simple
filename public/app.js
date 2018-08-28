436ff8c1a6ae2bfd0ea9f25dd142fba69f7e92b0
Andriy Golumbovsky <AGolumbovsky@gmail.com>
2018-08-28 07:34:47 -0400

    more stuff

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
 console.log("it works!")
+
+let buttClick = () => {
+    let userInput = document.getElementById("userInput").value;
+
+    alert(userInput);
+}
+
+let getIt = () => {
+
+    fetch('http://example.com/movies.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -6,6 +7,19 @@ const PORT = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://127.0.0.1/thisDB');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("db connected, mongoose did it");
+});
+
+
+var currentDate = new Date();
+var timestamp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
 app.listen(PORT, () => {
-    console.log("osla-s is listening on port" + PORT);
+    console.log("osla-s is listening on port" + PORT + "\n" + timestamp);
 })
