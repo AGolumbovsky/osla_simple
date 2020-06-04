dc25f43294de169f70cb88c7c317fcc84e92610d
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-06-04 00:40:10 -0400

    commented out mongoose stuff. to be deleted

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 const app = express();
 
@@ -7,15 +7,16 @@ const PORT = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://127.0.0.1/thisDB');
+// mongoose.connect('mongodb://127.0.0.1/thisDB');
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
+/*
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("db connected, mongoose did it");
 });
-
+*/
 
 app.get('/api/thisStuff', function(req, res) {
 
@@ -34,5 +35,5 @@ var currentDate = new Date();
 var timestamp = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 
 app.listen(PORT, () => {
-    console.log("osla-s is listening on port" + PORT + "\n" + timestamp);
+    console.log("osla-s is listening on port " + PORT + "\n" + timestamp);
 })
