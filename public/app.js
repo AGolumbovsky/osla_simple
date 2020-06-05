485303500cfa7dffc41fac9a6e139cca1c4cf185
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-06-04 20:26:01 -0400

    added bootstrap and basic elements

--- public/app.js
+++ public/app.js
@@ -2,27 +2,31 @@ console.log("it works!")
 
 let buttClick = () => {
     let userInput = document.getElementById("userInput").value;
-
     alert(userInput);
+    document.getElementById("result").innerHTML = userInput;
 }
 
-// some stuff on fetch() from the internets
-let getIt = () => {
+let findWord = () => {
 
-    fetch('http://example.com/movies.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-    });
+    // fetch('http://example.com/movies.json')
+    // .then(function(response) {
+    //     return response.json();
+    // })
+    // .then(function(myJson) {
+    //     console.log(JSON.stringify(myJson));
+    //     console.log("fetched something")
+    // });
+
+    document.getElementById("display-area").innerHTML = "TEST! FIND CLICKED";
 }
 
 // some more stuff on fetch() from the internets
 var url = 'https://example.com/profile';
 var data = {username: 'example'};
 
-let postIt = () => {
+let addWord = () => {
+
+    document.getElementById("display-area").innerHTML = "TEST! ADD CLICKED";
     
     fetch(url, {
     method: 'POST', // or 'PUT'
@@ -33,4 +37,12 @@ let postIt = () => {
     }).then(res => res.json())
     .then(response => console.log('Success:', JSON.stringify(response)))
     .catch(error => console.error('Error:', error));
+}
+
+let learnOsla = () => {
+    document.getElementById("display-area").innerHTML = "TEST! LEARN CLICKED";
+}
+
+let contactUs = () => {
+    document.getElementById("display-area").innerHTML = "TEST! CONTACT CLICKED";
 }
