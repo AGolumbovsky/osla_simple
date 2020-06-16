4fadf9150fbd96fc2711e81343368e0312b43f20
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-06-16 01:58:10 -0400

    added db_connect

---
+++ api/controllers/api_controller.js
@@ -0,0 +1,51 @@
+const { Pool } = require('pg');
+const pool = require('../config/db_connect');
+const cors = require('cors');
+const emailer = require('../emailer');
+
+module.exports = (app) => {
+
+    app.use(cors());
+    app.options('*', cors());
+
+    app.get('/api/latestReading', (req, res) => {
+
+        let queryText = `SELECT * FROM readings ORDER BY id DESC LIMIT 1`; // change to one last rdng
+        pool.query(queryText, (err, data) => {
+
+            if (err) {
+                throw err;
+            }
+            
+            console.log("SELECT query executed");
+            console.log(`apiController sent ${data.rows[0].reading}`);
+            res.send(data);
+
+            if (data.rows[0].reading <= 400) {
+                emailer(data.rows[0].reading);
+            }
+        })
+    });
+
+    app.post('/api/dummyPopulate', (req, res) => {
+
+        console.log("res is:", res.json());
+        let reading = 333;
+        let description = 'from apiController to the view';
+
+        let queryText = `INSERT INTO readings (reading, identifier)
+                        VALUES (${ reading }, 'from view to db dbl qs')`;
+        pool.query(queryText, (err, data) => {
+
+            if (err) {
+                throw err;
+            }
+
+            console.log("INSERT query executed", data);
+
+        });
+        
+                
+    });
+
+}
